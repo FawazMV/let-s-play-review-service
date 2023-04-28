9e6d98650f39a38823081b6bbd3eb1be9a9c1277
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-04-28 18:37:05 +0530

    review prblem solved

--- Controllers/controllers.js
+++ Controllers/controllers.js
@@ -5,7 +5,6 @@ import Reviewmodel from "../Models/Review_Model.js"
 export const reviewSubmit = async (req, res) => {
     try {
         const { id, text, rating, user } = req.body;
-        console.log(req.body)
         const newReview = Reviewmodel({
             rating, user, review: text, turf: id
         })
@@ -32,8 +31,7 @@ export const getReviews = async (req, res) => {
             // { $unwind: '$user' },
             { $project: { 'user.username': 1, rating: 1, review: 1, createdAt: 1, 'user.profile': 1, } },
         ])
-        console.log(data)
-        return res.status(200).json({data})
+        return res.status(200).json(data)
     }
     catch (error) {
         console.error(error)
